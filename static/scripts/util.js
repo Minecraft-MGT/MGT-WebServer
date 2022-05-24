06b481cf186bf035659c7af7ee0462ddf33f2a6c
noname28439 <dohle.leon@web.de>
2022-05-24 17:21:59 +0200

    cleaned up util.js

--- static/scripts/util.js
+++ static/scripts/util.js
@@ -1,111 +1,100 @@
-
-CLIENTINFO = {}
-
-
-async function acc_register(username, password){
-    let payload = {
-        "cmd": "user_register",
-        "args": {"username": username, "password": password}
-    }
-    let response = await apiRequest(payload);
-    alert(response["msg"]);
-    return response;
+//Helper functions
+function randint(max) {
+    return Math.floor(Math.random() * max);
 }
-
-async function acc_login(username, password){
-    let payload = {
-        "cmd": "user_login",
-        "args": {"username": username, "password": password}
-    }
-    let response = await apiRequest(payload);
-    if(response["ok"]){
-        saveAuthToken(response["authsync"]);
-        document.location.pathname = "/user/account";
-    }else{
-        alert("login failed...")
-    }
-    navbarUpdate();
-    return response;
-}
-
-async function currentUsername(){
-    let payload = {
-        "cmd": "session_username",
-        "args": {"token": localStorage.getItem("authtoken")}
-    }
-    let response = await apiRequest(payload);
-    if(response["ok"]){
-        return response["name"];
-    }else{
-        return "";
-    }
-}
-
-
-function authView(){
-    console.log("LS: "+localStorage.getItem("authtoken"))
-    console.log("CK: "+getCookie("authtoken"))
-}
-
-function saveAuthToken(token){
-    localStorage.setItem("authtoken", token);
-    loadAuthToken();
-}
-
-function loadAuthToken(){
-    if(localStorage.getItem("authtoken")!=null)
-        setCookie("authtoken", localStorage.getItem("authtoken"));
-    else
-        deleteCookie("authtoken");
-}
-
-function resetAuthToken(){
-    localStorage.removeItem("authtoken");
-    loadAuthToken();
-}
-
-function loggedIn(){
-    return localStorage.getItem("authtoken")!=null
-}
-
-//load token to cookies when lading the page
-
-window.addEventListener("load", ()=>{
-    loadAuthToken();
-    navbarUpdate();
-
-    if(!loggedIn())
-        for(ce of $(".requirelogin").get()){
-            $(ce).attr("disabled", "disabled");
-            $(ce).removeAttr("href");
-            ce.style.color = "rgb(100, 100, 100)";
-        }
-
-})
-
 function getCurrentTime(){
     var today = new Date();
     return today.getHours() + ":" + today.getMinutes();
 }
 
+
+
+//js executed on page load
+window.addEventListener("load", ()=>{
+    auth.loadAuthToken();
+    navbarUpdate();
+})
+
 async function navbarUpdate(){
     //build clientinfo
-    if(loggedIn()){//when logged in
-        CLIENTINFO["username"] = await currentUsername();
-        $("#navbar-acctile").html(CLIENTINFO.username);
-        $("#navbar-acctile").attr("href", "/user/account");
-    }
+    if(auth.loggedIn()){//when logged in
+        $("#navbar-acctile").html(currentUsername());
+        $("#navbar-acctile").attr("href", "/user/acc");
+    }else 
+        for(ce of $(".requirelogin").get()){
+            $(ce).attr("disabled", "disabled");
+            $(ce).removeAttr("href");
+            ce.style.color = "rgb(100, 100, 100)";
+        }
   }
 
 
-converter = {
-    "timeToDayMin": (time)=>{
-        return Number(time.split(":")[0])*60+Number(time.split(":")[1])
+//account/session controll
+
+var acc = {
+    "register": async function(username, password){
+        let payload = {
+            "cmd": "user_register",
+            "args": {"username": username, "password": password}
+        }
+        let response = await apiRequest(payload);
+        alert(response["msg"]);
+        return response;
     },
-    "dayMinToTime": (daysec)=>{
-        return String(Math.floor(daysec/60))+":"+Math.floor(String(daysec%60))
+    "login": async function (username, password){
+        let payload = {
+            "cmd": "user_login",
+            "args": {"username": username, "password": password}
+        }
+        let response = await apiRequest(payload);
+        if(response["ok"]){
+            auth.saveAuthToken(response["authsync"]);
+            document.location.pathname = "/user/account";
+        }else{
+            alert("login failed...")
+        }
+        navbarUpdate();
+        return response;
     }
 }
+
+var auth = {
+    "view": function(){
+        console.log("LS: "+localStorage.getItem("authtoken"))
+        console.log("CK: "+getCookie("authtoken"))
+        console.log("UN: "+localStorage.getItem("username"))
+    },
+    "saveAuthToken": function (token){
+        localStorage.setItem("authtoken", token);
+        auth.loadAuthToken();
+    },
+    "loadAuthToken": function (){
+        if(localStorage.getItem("authtoken")!=null)
+            setCookie("authtoken", localStorage.getItem("authtoken"));
+        else
+            deleteCookie("authtoken");
+    },
+    "resetSession": function (){
+        localStorage.removeItem("authtoken");
+        auth.loadAuthToken();
+        localStorage.removeItem("username");
+    },
+    "loggedIn": function (){
+        return localStorage.getItem("authtoken")!=null
+    },
+    "setUsername": function (username){
+        localStorage.setItem("username", username);
+    },
+    "currentUsername": function (){
+        if(localStorage.getItem("username") == null) return null;
+        return localStorage.getItem("username");
+    }
+}
+
+
+
+
+
 
 // send all Errors as debug to the server
 var foundErrors = []
@@ -117,7 +106,3 @@ window.addEventListener("error", async function (e) {
     }
     return false;
  })
-
- function randint(max) {
-    return Math.floor(Math.random() * max);
-  }
