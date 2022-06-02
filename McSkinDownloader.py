916796fc647961afaf76cd17a7755184634016f5
Noname <62444462+noname28439@users.noreply.github.com>
2022-06-02 09:00:45 +0200

    mc downloader fix

--- McSkinDownloader.py
+++ McSkinDownloader.py
@@ -28,27 +28,32 @@ def download(username, outputpath):
     if r.status_code != 200:
         fail("Could not retrieve user ID for {username}".format(username=username),
              "{0} {1}".format(r.status_code, userid_url.format(username=username)))
+        return
     userid = r.json()['id']
 
     r = get_url(userinfo_url.format(userid=userid))
     if r.status_code != 200:
         fail("Failed to download user info for {username}".format(username=username),
              "{0} {1}".format(r.status_code, userinfo_url.format(userid=userid)))
+        return
     userinfo = r.json()
     texture_info = find_texture_info(userinfo['properties'])
     if texture_info is None:
         fail("Failed to find texture info for {username}".format(username=username),
              userinfo)
+        return
 
     try:
         skin_url = texture_info['textures']['SKIN']['url']
     except:
         fail("Failed to find texture info for {username}".format(username=username),
              texture_info)
+        return
     r = get_url(skin_url, stream=True)
     if r.status_code != 200:
         fail("Could not download skin for {username}".format(username=username),
              "{0} {1}".format(r.status_code, skin_url))
+        return
 
     with open(outputpath+username+".png", 'wb') as f:
         shutil.copyfileobj(r.raw, f)
